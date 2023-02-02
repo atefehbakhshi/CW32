34b3be8fd75086679654fb6f95084ed68cbbc3d1
Atefeh <at.bakhshi1373@gmail.com>
2023-02-02 21:23:54 +0330

    feat: add required attribute in to inputs field

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,37 @@
+import { useState } from "react";
+import PasswordVisibility from "./PasswordVisibility";
+
 export default function Login() {
+  const [inputType, setInputType] = useState("password");
+
+  const loginForm = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="flex gap-4 flex-col">
-      <h1 className="text-center">خوش آمدید</h1>
-      <input
-        type="email"
-        placeholder="پست الکترونیک"
-        className="bg-transparent border-[#44535a] border-2 p-1 "
-      />
-      <div className="border-[#44535a] border-2 p-1 flex items-center justify-between">
+    <div>
+      <h1 className="text-center text-xl mb-4">خوش آمدید</h1>
+      <form className="flex gap-4 flex-col" onSubmit={loginForm}>
         <input
-          type="password"
-          placeholder="کلمه عبور"
-          className="bg-transparent"
+          type="email"
+          placeholder="پست الکترونیک"
+          required
+          className="bg-transparent border-[#44535a] border-2 p-1 "
         />
-        <span>&#128065; &#128274;</span>
-      </div>
-      <p>فراموش کردید ؟</p>
-      <button className="bg-[#1ab08a] p-2">ورود</button>
+        <div className=" border-[#44535a] border-2 p-1 flex items-center justify-between">
+          <input
+            type={inputType}
+            placeholder="کلمه عبور"
+            required
+            className="bg-transparent w-11/12"
+          />
+          <PasswordVisibility setInputType={setInputType} />
+        </div>
+        <p className="text-xs text-[#1ab08a] ">فراموش کردید ؟</p>
+        <button className="bg-[#1ab08a] p-2" type="submit">
+          ورود
+        </button>
+      </form>
     </div>
   );
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,47 +1,63 @@
-export default function Register() {
-  return (
-    <div className="flex gap-4 flex-col">
-      <h1 className="text-center text-2xl">رایگان ثبت نام کنید</h1>
-      <div className="flex gap-2 ">
-        <input
-          type="text"
-          placeholder="نام"
-          className="bg-transparent border-[#44535a] border-2 p-1 w-1/2 "
-        />
-        <input
-          type="text"
-          placeholder="نام خانوادگی"
-          className="bg-transparent border-[#44535a] border-2 p-1 w-1/2 "
-        />
-      </div>
-      <div>
-        <select className="bg-transparent border-[#44535a] border-2 p-1">
-          <option value="kardani" className="bg-gray-500">
-            کاردانی
-          </option>
-          <option value="karshenasi" className="bg-gray-500">
-            کارشناسی
-          </option>
-          <option value="karshenasi-arshad" className="bg-gray-500">
-            کارشناسی ارشد
-          </option>
-        </select>
-      </div>
-      <input
-        type="email"
-        placeholder="پست الکترونیک"
-        className="bg-transparent border-[#44535a] border-2 p-1 "
-      />
-      <div className="border-[#44535a] border-2 p-1 flex items-center justify-between">
-        <input
-          type="password"
-          placeholder="کلمه عبور"
-          className="bg-transparent"
-        />
-        <span>&#128065; &#128274;</span>
-      </div>
+import { useState } from "react";
+import PasswordVisibility from "./PasswordVisibility";
 
-      <button className="bg-[#1ab08a] p-2">ثبت نام</button>
+export default function Register() {
+  const [inputType, setInputType] = useState("password");
+
+  const registerForm = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <h1 className="text-center text-xl mb-4">رایگان ثبت نام کنید</h1>
+      <form className="flex gap-4 flex-col" onSubmit={registerForm}>
+        <div className="flex gap-2 ">
+          <input
+            type="text"
+            placeholder="نام"
+            required
+            className="bg-transparent border-[#44535a] border-2 p-1 w-1/2 "
+          />
+          <input
+            type="text"
+            placeholder="نام خانوادگی"
+            required
+            className="bg-transparent border-[#44535a] border-2 p-1 w-1/2 "
+          />
+        </div>
+        <div>
+          <select className="bg-transparent border-[#44535a] border-2 p-1">
+            <option value="kardani" className="bg-gray-500">
+              کاردانی
+            </option>
+            <option value="karshenasi" className="bg-gray-500">
+              کارشناسی
+            </option>
+            <option value="karshenasi-arshad" className="bg-gray-500">
+              کارشناسی ارشد
+            </option>
+          </select>
+        </div>
+        <input
+          type="email"
+          placeholder="پست الکترونیک"
+          required
+          className="bg-transparent border-[#44535a] border-2 p-1 "
+        />
+        <div className="border-[#44535a] border-2 p-1 flex items-center justify-between">
+          <input
+            type={inputType}
+            placeholder="کلمه عبور"
+            required
+            className="bg-transparent w-11/12"
+          />
+          <PasswordVisibility setInputType={setInputType} />
+        </div>
+        <button className="bg-[#1ab08a] p-2" type="submit">
+          ثبت نام
+        </button>
+      </form>
     </div>
   );
 }
